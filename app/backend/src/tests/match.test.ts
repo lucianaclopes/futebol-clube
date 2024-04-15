2351230e68204df408937083ebede8b8cbaf7f61
lucianaclopes <lucianacls@outlook.com>
2024-04-14 21:34:46 -0300

    adiciona testes para matches

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -5,7 +5,7 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import SequelizeMatch from '../database/models/SequelizeMatch';
-import { match1, match2, matches } from './mocks/match.mock';
+import { allMatches, matchesDone, matchesInProgress } from './mocks/match.mock';
 
 
 chai.use(chaiHttp);
@@ -14,9 +14,38 @@ const { expect } = chai;
 
 describe('Match Test', function() {
   it('Should return all matches', async function() {
-    sinon.stub(SequelizeMatch, 'findAll').returns(matches as any);
+    sinon.stub(SequelizeMatch, 'findAll').returns(allMatches as any);
     const { status, body } = await chai.request(app).get('/matches');
     expect(status).to.equal(200);
-    expect(body).to.deep.equal(matches);
+    expect(body).to.deep.equal(allMatches);
   });
+
+  it('Should return all matches in progress', async function() {
+    sinon.stub(SequelizeMatch, 'findAll').callsFake(async (options: any) => {
+      if (options.where && options.where.inProgress === true) {
+        return matchesInProgress as any;
+      } else {
+        return allMatches as any;
+      }
+    });
+
+    const { status, body } = await chai.request(app).get('/matches?inProgress=true');
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matchesInProgress);
+  });
+
+  it('Should return all matches done', async function() {
+    sinon.stub(SequelizeMatch, 'findAll').callsFake(async (options: any) => {
+      if (options.where && options.where.inProgress === false) {
+        return matchesDone as any;
+      } else {
+        return allMatches as any;
+      }
+    });
+
+    const { status, body } = await chai.request(app).get('/matches?inProgress=false');
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matchesDone);
+  });
+  afterEach(sinon.restore);
 });
--- app/backend/src/tests/mocks/match.mock.ts
+++ app/backend/src/tests/mocks/match.mock.ts
@@ -16,6 +16,18 @@ const match2 = {
   inProgress: false 
 }
 
-const matches = [match1, match2];
+const match3 = {
+  id: 3, 
+  homeTeamId: 16, 
+  homeTeamGoals: 1, 
+  awayTeamId: 8, 
+  awayTeamGoals: 2, 
+  inProgress: true
 
-export { matches, match1, match2 };
+}
+
+const allMatches = [match1, match2, match3];
+const matchesInProgress = [match3];
+const matchesDone = [match1, match2];
+
+export { allMatches, match1, match2, matchesDone, matchesInProgress};
